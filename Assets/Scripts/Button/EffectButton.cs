8d9441a36f37f5eee72b3dae751bb735db67548f
VinPhong <dongphongvin0909@gmail.com>
2024-07-15 23:00:56 +0700

    + import project

---
+++ Assets/Scripts/Button/EffectButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectButton : BaseButton
+{
+    protected override void OnButtonClick()
+    {
+        base.OnButtonClick();
+    }
+}
---
+++ Assets/Scripts/Button/ProcessButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcessButton : BaseButton
+{
+    protected override void OnButtonClick()
+    {
+        base.OnButtonClick();
+    }
+}
