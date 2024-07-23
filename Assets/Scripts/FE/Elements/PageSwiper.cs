6c8e25b7343db7061cc7da9e8b07ca2f9619f819
Duc Minh <phamducminh1302@gmail.com>
2024-07-24 00:30:39 +0700

    + add PageSwiper

--- Assets/Scripts/FE/Elements/PageSwiper.cs
+++ Assets/Scripts/FE/Elements/PageSwiper.cs
@@ -1,11 +1,46 @@
 using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PageSwiper : MonoBehaviour
+public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    
+    [SerializeField] private RectTransform contentTab;
+    [SerializeField] private float widthChildren;
+    float difference;
+
+    private void Start()
+    {
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        difference = eventData.pressPosition.x - eventData.position.x;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        float newPosition = 0;
+        float currentPosX = contentTab.anchoredPosition.x;
+
+        // Luot tu phai sang trai
+        if (difference > 0)
+        {
+            //Neu den item cuoi cung thi dung
+            if (widthChildren >= (contentTab.rect.width - Mathf.Abs(currentPosX)))
+                return;
+            newPosition = currentPosX - widthChildren;
+        }
+        
+        //Luot tu trai sang phai
+        if (difference < 0)
+        {
+            //Neu den item dau tien thi dung
+            if (contentTab.anchoredPosition.x == 0)
+                return;
+            newPosition = currentPosX + widthChildren;
+        }
+
+        contentTab.DOAnchorPosX(newPosition, .15f);
+    }
 }
--- Assets/Scripts/FE/Panel/TabPanel.cs
+++ Assets/Scripts/FE/Panel/TabPanel.cs
@@ -7,12 +7,13 @@ using UnityEngine.UI;
 public class TabPanel : BasePanel
 {
     [Header("Elements")]
-    [SerializeField] private RectTransform rect;
+    [SerializeField] private RectTransform containerTab;
     [SerializeField] private RectTransform tabContainer;
+    PageSwiper swiper;
 
     [Header("Properties")]
     [SerializeField] private float duration = .1f;
-    [SerializeField] private Ease effect;
+    [SerializeField] private Ease fadeTtransition;
 
     protected override void Awake()
     {
@@ -20,16 +21,17 @@ public class TabPanel : BasePanel
 
         UIManager.Instance.ResgisterPanel(UIPanelType.Tab, this);
         canvasGroup = GetComponent<CanvasGroup>();
+        swiper = GetComponentInChildren<PageSwiper>();
 
-        rect.localScale = new Vector3(.1f, .1f, .1f);
+        containerTab.localScale = new Vector3(.1f, .1f, .1f);
     }
 
     public override void Hide()
     {
         base.Hide();
         canvasGroup.DOFade(0, duration);
-        rect.DOScale(new Vector3(.1f, .1f, .1f), duration)
-            .SetEase(effect)
+        containerTab.DOScale(new Vector3(.1f, .1f, .1f), duration)
+            .SetEase(fadeTtransition)
             .OnComplete(() =>
             {
                 Deactive();
@@ -40,8 +42,8 @@ public class TabPanel : BasePanel
     {
         base.Show();
         canvasGroup.DOFade(1, duration);
-        rect.DOScale(Vector3.one, duration)
-            .SetEase(effect);
+        containerTab.DOScale(Vector3.one, duration)
+            .SetEase(fadeTtransition);
     }
 
     public void ShowTab(UITab tabType)
